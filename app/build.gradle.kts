c9f85f5d2c737536bd5668f7f4a304d7e942033f
Felitendo <Felendo@web.de>
2024-07-19 23:55:36 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "io.github.felitendo.xposed"
         minSdk = 24
         targetSdk = 33
-        versionCode = v0.0.1
-        versionName = "v0.0.1"
+        versionCode = 0.0.2
+        versionName = "v0.0.2"
     }
 
     buildTypes {
